91d678a1d45415411b0502fb10e933fe80cefd41
jwinstead <>
2007-03-09 01:26:58 +0000

    Update copyright notices

--- VersionInfo.h
+++ VersionInfo.h
@@ -1,28 +1,30 @@
-/* Copyright (C) 2000-2005 MySQL AB
+/*
+  Copyright (C) 2000-2007 MySQL AB
 
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
-   (at your option) any later version.
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of version 2 of the GNU General Public License as
+  published by the Free Software Foundation.
 
-   There are special exceptions to the terms and conditions of the GPL as it
-   is applied to this software. View the full text of the exception in file
-   EXCEPTIONS in the directory of this software distribution.
+  There are special exceptions to the terms and conditions of the GPL
+  as it is applied to this software. View the full text of the exception
+  in file LICENSE.exceptions in the top-level directory of this software
+  distribution.
 
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
 
 /* When changing, also change configure.in */
 #define SETUP_VERSION         "3.51.14"
 #define DRIVER_VERSION        "03.51.14"
 
-#define MYODBC_VERSION        "3.51.14"  
+#define MYODBC_VERSION        "3.51.14"
 #define MYODBC_FILEVER        3,51,14,0
 #define MYODBC_PRODUCTVER     3,51,14,0
 #define MYODBC_STRFILEVER     "3, 51, 14, 0\0"
