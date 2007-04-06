921415a6bf9cee0194a938caf1251a7d649e7fa9
jwinstead <>
2007-04-06 02:54:30 +0000

    Rename my_ts to my_datetime to make it more clear what it is testing, and move the regression test for Bug #25846 into it so it actually gets used.

--- test/my_datetime.c
+++ test/my_datetime.c
@@ -226,10 +226,111 @@ DECLARE_TEST(t_tstotime1)
 }
 
 
+DECLARE_TEST(t_bug25846)
+{
+    SQLRETURN   rc;
+    SQLINTEGER  narg;
+
+    SQLSMALLINT          column_count;
+    SQLINTEGER           my_time_cb;
+    SQLINTEGER           my_date_cb;
+    SQL_TIMESTAMP_STRUCT my_time_ts;
+    SQL_TIMESTAMP_STRUCT my_date_ts;
+
+    struct tm *current_ts;
+    time_t    sec_time;
+
+    /* initialize data */
+    SQLExecDirect(hstmt, "DROP TABLE my_time_tab", SQL_NTS);
+
+    rc= SQLExecDirect(hstmt, "CREATE TABLE my_time_tab (ID INT PRIMARY KEY, \
+                                                        my_time TIME, \
+                                                        my_date DATE)",
+                                                        SQL_NTS);
+    mystmt(hstmt, rc);
+
+    /* Insert 15:45:30 into TIME column */
+    rc= SQLExecDirect(hstmt, "INSERT INTO my_time_tab (ID, my_time, my_date) \
+                             VALUES (0, '15:45:30', CURDATE())",
+                             SQL_NTS);
+    mystmt(hstmt, rc);
+
+    rc= SQLExecDirect(hstmt, "SELECT ID, my_time, my_date FROM my_time_tab",
+                             SQL_NTS);
+    mystmt(hstmt, rc);
+
+    rc= SQLNumResultCols(hstmt, &column_count);
+    mystmt(hstmt, rc);
+
+    printMessage("Columns count: %d \n\n", column_count);
+
+    /* Bind the TIMESTAMP buffer for TIME column */
+    rc= SQLBindCol(hstmt, 2, SQL_C_TIMESTAMP, &my_time_ts, sizeof(my_time_ts),
+                   &my_time_cb);
+    mystmt(hstmt, rc);
+
+    /* Bind the TIMESTAMP buffer for DATE column */
+    rc= SQLBindCol(hstmt, 3, SQL_C_TIMESTAMP, &my_date_ts, sizeof(my_date_ts),
+                   &my_date_cb);
+    mystmt(hstmt, rc);
+
+    rc= SQLFetch(hstmt);
+    mystmt(hstmt, rc);
+
+    sec_time= time(NULL);
+    current_ts= localtime(&sec_time);
+
+    printMessage("CURRENT  DATE\n  YEAR: %d\n  MON: %d\n  DAY: %d \n\n",
+                 current_ts->tm_year + 1900,   /* year starts from 1900 */
+                 current_ts->tm_mon + 1,       /* month starts from 0 */
+                 current_ts->tm_mday);         /* days are ok */
+
+    printMessage("TIME Column\n");
+    printMessage("RETURNED DATE\n  YEAR: %d\n  MON: %d\n  DAY: %d \n",
+                 my_time_ts.year, my_time_ts.month, my_time_ts.day);
+
+    printMessage("RETURNED TIME\n  HOUR: %d\n  MIN: %d\n  SEC: %d \n\n",
+                 my_time_ts.hour, my_time_ts.minute, my_time_ts.hour);
+
+    printMessage("DATE Column\n");
+    printMessage("RETURNED DATE\n  YEAR: %d\n  MON: %d\n  DAY: %d \n",
+                 my_date_ts.year, my_date_ts.month, my_date_ts.day);
+
+    myassert(my_time_ts.hour   == 15 &&
+             my_time_ts.minute == 45 &&
+             my_time_ts.second == 30);
+
+    /* Most cases when we are not testin at 12:00 AM */
+    if (my_time_ts.day == current_ts->tm_mday)
+        myassert((current_ts->tm_year + 1900) == my_time_ts.year &&
+                  my_time_ts.year             == my_date_ts.year &&
+                  (current_ts->tm_mon + 1)    == my_time_ts.month &&
+                  my_time_ts.month            == my_date_ts.month &&
+                  current_ts->tm_mday         == my_time_ts.day &&
+                  my_time_ts.day              == my_date_ts.day);
+    else
+        myassert((current_ts->tm_year + 1900) >= my_time_ts.year &&
+                  my_time_ts.year             >= my_date_ts.year &&
+                  (current_ts->tm_mon + 1)    >= my_time_ts.month &&
+                  my_time_ts.month            >= my_date_ts.month &&
+                  current_ts->tm_mday         >= my_time_ts.day &&
+                  my_time_ts.day              >= my_date_ts.day);
+
+    SQLFreeStmt(hstmt, SQL_UNBIND);
+    SQLFreeStmt(hstmt, SQL_CLOSE);
+
+    SQLExecDirect(hstmt, "DROP TABLE my_time_tab", SQL_NTS);
+    SQLFreeStmt(hstmt, SQL_CLOSE);
+
+  return OK;
+}
+
+
 BEGIN_TESTS
   ADD_TEST(my_ts)
   ADD_TEST(t_tstotime)
   ADD_TEST(t_tstotime1)
+  ADD_TEST(t_bug25846)
 END_TESTS
 
 
