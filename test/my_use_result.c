242ed0ec65301031a386d735967d4b1c9e7b73bd
jwinstead <>
2007-04-12 06:49:30 +0000

    Complete conversion of my_use_result to odbctap

--- test/my_use_result.c
+++ test/my_use_result.c
@@ -22,17 +22,12 @@
 
 #include "odbctap.h"
 
-SQLINTEGER my_max_rows = 1000;
-clock_t t_start, t_end;
-SQLDOUBLE my_time;
+SQLINTEGER my_max_rows= 10000;
 
 /* Clean data */
 DECLARE_TEST(t_clean_data)
 {
-    SQLRETURN rc;
-
-    rc = SQLExecDirect(hstmt,"drop database client_odbc_test1",SQL_NTS);
-    mystmt(hstmt,rc);
+  ok_sql(hstmt, "DROP TABLE IF EXISTS t_use_result");
 
   return OK;
 }
@@ -41,138 +36,88 @@ DECLARE_TEST(t_clean_data)
 /* Initialize data */
 DECLARE_TEST(t_init_data)
 {
-    SQLRETURN rc;
-    SQLINTEGER i;
-    SQLCHAR    ch[]="MySQL AB";
+  SQLINTEGER  i;
+  SQLCHAR     ch[]= "MySQL AB";
 
-    SQLExecDirect(hstmt,"drop database if exists client_odbc_test1",SQL_NTS);
+  ok_sql(hstmt, "DROP TABLE IF EXISTS t_use_result");
+  ok_sql(hstmt, "CREATE TABLE t_use_result (id INT, name CHAR(10))");
 
-    rc = SQLExecDirect(hstmt,"create database client_odbc_test1",SQL_NTS);
-    mystmt(hstmt,rc);
+  ok_stmt(hstmt, SQLPrepare(hstmt, (SQLCHAR *)
+                            "INSERT INTO t_use_result VALUES (?,?)", SQL_NTS));
 
-    SQLExecDirect(hstmt,"drop table client_odbc_test1.t_use_result",SQL_NTS);
+  ok_stmt(hstmt, SQLBindParameter(hstmt, 1, SQL_PARAM_INPUT, SQL_C_LONG,
+                                  SQL_INTEGER, 0, 0, &i, 0, NULL));
 
-    rc = SQLExecDirect(hstmt,"create table client_odbc_test1.t_use_result(id int,name char(10))",SQL_NTS);
-    mystmt(hstmt,rc);
+  ok_stmt(hstmt, SQLBindParameter(hstmt, 2, SQL_PARAM_INPUT, SQL_C_CHAR,
+                                  SQL_CHAR, 0, 0, ch, sizeof(ch), NULL));
 
-    rc = SQLPrepare(hstmt,"insert into client_odbc_test1.t_use_result values(?,?)",SQL_NTS);
-    mystmt(hstmt,rc);
+  for (i= 1; i <= my_max_rows; i++)
+    ok_stmt(hstmt, SQLExecute(hstmt));
 
-    rc = SQLBindParameter(hstmt,1,SQL_PARAM_INPUT, SQL_C_ULONG,
-                          SQL_INTEGER,0,0,&i,0,NULL);
-    mystmt(hstmt,rc);
-
-    rc = SQLBindParameter(hstmt,2,SQL_PARAM_INPUT, SQL_C_CHAR,
-                          SQL_CHAR,0,0,&ch,10,NULL);
-    mystmt(hstmt,rc);
-
-    printMessage("\n inserting '%d' rows (it will take few minutes)\n",my_max_rows);
-    t_start = clock();
-
-    for ( i = 1; i <= my_max_rows; i++ )
-    {
-        rc = SQLExecute(hstmt);
-        mystmt(hstmt,rc);
-        if (!(i % 1000)) printMessage(" \r %d",i);
-    }
-    t_end = clock();
-
-    my_time = difftime(t_end,t_start)/CLOCKS_PER_SEC;   
-    printMessage("\n total of '%d' rows inserted in %5.3f SECS",my_max_rows,my_time);
-
-    SQLFreeStmt(hstmt,SQL_RESET_PARAMS);
-    SQLFreeStmt(hstmt,SQL_CLOSE);
-
-    rc = SQLEndTran(SQL_HANDLE_DBC,hdbc,SQL_COMMIT);
-    mycon(hdbc,rc);
+  ok_stmt(hstmt, SQLFreeStmt(hstmt, SQL_RESET_PARAMS));
+  ok_stmt(hstmt, SQLFreeStmt(hstmt, SQL_CLOSE));
 
   return OK;
 }
 
 
 /* Fetch and count the time */
-SQLINTEGER t_fetch_data(SQLHDBC hdbc,SQLHSTMT hstmt)
+int t_fetch_data(SQLHSTMT hstmt)
 {
-    SQLRETURN   rc;
-    SQLINTEGER  row_count=0;
+  SQLRETURN rc;
+  SQLINTEGER row_count= 0;
 
-    /* set row size as 1 */
-    rc = SQLSetStmtAttr(hstmt,SQL_ATTR_ROW_ARRAY_SIZE,(SQLPOINTER)1,0);
-    mystmt(hstmt,rc);
+  ok_sql(hstmt, "SELECT * FROM t_use_result");
 
-    SQLSetStmtAttr(hstmt, SQL_ATTR_CONCURRENCY, (SQLPOINTER) SQL_CONCUR_ROWVER, 0);
-    SQLSetStmtAttr(hstmt, SQL_ATTR_CURSOR_TYPE, (SQLPOINTER) SQL_CURSOR_FORWARD_ONLY, 0);
+  rc= SQLFetch(hstmt);
+  while (rc == SQL_SUCCESS || rc == SQL_SUCCESS_WITH_INFO)
+  {
+    row_count++;
+    rc= SQLFetch(hstmt);
+  }
 
-    t_start = clock();
-    rc = SQLExecDirect(hstmt,"select * from client_odbc_test1.t_use_result",SQL_NTS);
-    mystmt(hstmt,rc);
-    t_end = clock();
+  ok_stmt(hstmt, SQLFreeStmt(hstmt, SQL_UNBIND));
+  ok_stmt(hstmt, SQLFreeStmt(hstmt, SQL_CLOSE));
 
-    my_time = difftime(t_end,t_start)/CLOCKS_PER_SEC;   
-    printMessage("\n SELECT statement executed in %5.3f SECS",my_time);
+  is_num(row_count, my_max_rows);
 
-    t_start = clock();
-    rc = SQLFetch(hstmt);
-    while (rc == SQL_SUCCESS || rc == SQL_SUCCESS_WITH_INFO)
-    {
-        row_count++;
-        rc = SQLFetch(hstmt);
-    }
-    t_end = clock();  
-
-    my_time = difftime(t_end,t_start)/CLOCKS_PER_SEC; 
-    printMessage("\n total '%d' rows fetched in %5.3f SECS",row_count,my_time);
-
-    SQLFreeStmt(hstmt,SQL_UNBIND);    
-    SQLFreeStmt(hstmt,SQL_CLOSE);  
-    return(row_count);
+  return OK;
 }
 
 
 /* making use of mysql_use_result */
 DECLARE_TEST(t_use_result)
 {
-    SQLHENV   henv1;
-    SQLHDBC   hdbc1;
-    SQLHSTMT  hstmt1;
-    SQLCHAR   conn[255];
-    SQLINTEGER option = 131072L * 8;
+  SQLHENV   henv1;
+  SQLHDBC   hdbc1;
+  SQLHSTMT  hstmt1;
+  int ret;
 
-    sprintf(conn,"DSN=%s;UID=%s;PWD=%s;OPTION=%d",
-            mydsn,myuid,mypwd,option);
-    if (mysock != NULL)
-    {
-      strcat(conn, ";SOCKET=");
-      strcat(conn, mysock);
-    }
-    mydrvconnect(&henv1,&hdbc1,&hstmt1,conn);
-    my_assert(t_fetch_data(hdbc1,hstmt1) == my_max_rows);
-    mydisconnect(&henv1,&hdbc1,&hstmt1);
+  SET_DSN_OPTION(131072L * 8);
 
-  return OK;
+  alloc_basic_handles(&henv1, &hdbc1, &hstmt1);
+  ret= t_fetch_data(hstmt1);
+  free_basic_handles(&henv1, &hdbc1, &hstmt1);
+
+  return ret;
 }
 
 /* making use of mysql_store_result */
 DECLARE_TEST(t_store_result)
 {
-    SQLHENV   henv1;
-    SQLHDBC   hdbc1;
-    SQLHSTMT  hstmt1;
-    SQLCHAR   conn[255];
-    SQLINTEGER option = 3;
+  SQLHENV   henv1;
+  SQLHDBC   hdbc1;
+  SQLHSTMT  hstmt1;
+  int ret;
 
-    sprintf(conn,"DSN=%s;UID=%s;PWD=%s;OPTION=%d",
-            mydsn,myuid,mypwd,option);
-    if (mysock != NULL)
-    {
-      strcat(conn, ";SOCKET=");
-      strcat(conn, mysock);
-    }
-    mydrvconnect(&henv1,&hdbc1,&hstmt1,conn);
-    my_assert(t_fetch_data(hdbc1,hstmt1) == my_max_rows);
-    mydisconnect(&henv1,&hdbc1,&hstmt1);
+  SET_DSN_OPTION(3);
 
-  return OK;
+  alloc_basic_handles(&henv1, &hdbc1, &hstmt1);
+  /* we re-use option here */
+  ret= t_fetch_data(hstmt1);
+  free_basic_handles(&henv1, &hdbc1, &hstmt1);
+
+  return ret;
 }
 
 
