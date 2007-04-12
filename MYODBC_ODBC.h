93933cd1dcb3677a85604e9c36ffb5408e025141
jwinstead <>
2007-04-12 02:49:12 +0000

    Fix indentation -- # must be in first column

--- MYODBC_ODBC.h
+++ MYODBC_ODBC.h
@@ -4,38 +4,35 @@
 #define ODBCVER 0x0351
 
 #ifdef _UNIX_
-    #include <ltdl.h>
-    #include <sql.h>
-    #include <sqlext.h>
-    #ifdef USE_IODBC
-        #include <iodbcinst.h>
-    #else
-        #include <odbcinst.h>
-    #endif
+# include <ltdl.h>
+# include <sql.h>
+# include <sqlext.h>
+# ifdef USE_IODBC
+#  include <iodbcinst.h>
+# else
+#  include <odbcinst.h>
+# endif
 
-    #ifndef SYSTEM_ODBC_INI
-        #define BOTH_ODBC_INI ODBC_BOTH_DSN
-        #define USER_ODBC_INI ODBC_USER_DSN
-        #define SYSTEM_ODBC_INI ODBC_SYSTEM_DSN
-    #endif
+# ifndef SYSTEM_ODBC_INI
+#  define BOTH_ODBC_INI ODBC_BOTH_DSN
+#  define USER_ODBC_INI ODBC_USER_DSN
+#  define SYSTEM_ODBC_INI ODBC_SYSTEM_DSN
+# endif
 
-    /*
-      If SQL_API is not defined, define it, unixODBC doesn't have this
-    */
-    #if !defined(SQL_API)
-        #define SQL_API
-    #endif
+/* If SQL_API is not defined, define it, unixODBC doesn't have this */
+# if !defined(SQL_API)
+#  define SQL_API
+# endif
 #else
-    #include <windows.h>
-    #ifndef RC_INVOKED
-        #pragma pack(1)
-    #endif
-    
-    #include <sqltypes.h>
-    #include <sql.h>
-    #include <sqlext.h>
-    #include <odbcinst.h>
+# include <windows.h>
+# ifndef RC_INVOKED
+#  pragma pack(1)
+# endif
+
+# include <sqltypes.h>
+# include <sql.h>
+# include <sqlext.h>
+# include <odbcinst.h>
 #endif
 
 #endif
-
