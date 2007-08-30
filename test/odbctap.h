0e67b53efc5cefcb139dcacfa141da60e6e45378
jwinstead <>
2007-08-30 03:13:23 +0000

    Fix calculation of length of wchar_t * for W() macro.

--- test/odbctap.h
+++ test/odbctap.h
@@ -953,7 +953,8 @@ int free_basic_handles(SQLHENV *henv,SQLHDBC *hdbc, SQLHSTMT *hstmt)
 /**
  Helper for possibly converting a (wchar_t *) to a (SQLWCHAR *)
 */
-#define W(string) dup_wchar_t_as_sqlwchar((string), sizeof(string))
+#define W(string) dup_wchar_t_as_sqlwchar((string), \
+                                          sizeof(string) / sizeof(wchar_t))
 #define WL(string, len) dup_wchar_t_as_sqlwchar((string), (len))
 
 
