7e48c7a01cf386fe4e03e0ff520457b69bec4c71
Kent Boortz <kent.boortz@sun.com>
2009-11-04 18:20:13 +0100

    Copyright header corrections

--- installer/myodbc3i.c
+++ installer/myodbc3i.c
@@ -1,9 +1,8 @@
 /* Copyright (C) 2000-2007 MySQL AB
 
    This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
-   (at your option) any later version.
+   it under the terms of version 2 of the GNU General Public License as
+   published by the Free Software Foundation.
 
    There are special exceptions to the terms and conditions of the GPL as it
    is applied to this software. View the full text of the exception in file
