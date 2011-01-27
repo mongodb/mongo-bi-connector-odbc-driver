a0e83ca595e80b9b01968aa4ca3bf8383f10e40a
Rafal Somla <rafal.somla@oracle.com>
2011-01-27 23:03:32 +0100

    Remove PAUSE commands from scripts because they are not needed and cause troubles in automated builds.

--- Install.bat
+++ Install.bat
@@ -6,9 +6,6 @@ REM
 REM         This exists for those working with the Windows source
 REM         distribution.
 REM
-REM         Use this to copy the driver and supporting files
-REM         to the system directory and register the driver.
-REM
 REM \sa     README.win
 REM
 REM #########################################################
@@ -18,41 +15,78 @@ IF EXIST %windir%\system\nul   SET installdir=%windir%\system
 IF EXIST %windir%\system32\nul SET installdir=%windir%\system32
 IF %installdir%==none GOTO :doError5
 
-IF EXIST %installdir%\myodbc-installer.exe GOTO :doError4
+IF "%1"=="1" GOTO :doDebug
+IF "%1"=="0" GOTO :doNormal
+GOTO doSyntax
+
+:doNormal
+IF EXIST %installdir%\myodbc3i.exe GOTO :doError4
 
 REM ****
 REM * Find out the bin/lib directory, or use default
 REM ****
 SET libdir=lib
 SET bindir=bin
-IF EXIST lib\release\myodbc5.lib         SET libdir=lib\release
-IF EXIST lib\relwithdebinfo\myodbc5.lib  SET libdir=lib\relwithdebinfo
-IF EXIST bin\release\myodbc-installer.exe        SET bindir=bin\release
-IF EXIST bin\relwithdebinfo\myodbc-installer.exe SET bindir=bin\relwithdebinfo
+IF EXIST lib\release\myodbc3.lib         SET libdir=lib\release
+IF EXIST lib\relwithdebinfo\myodbc3.lib  SET libdir=lib\relwithdebinfo
+IF EXIST bin\release\myodbc3i.exe        SET bindir=bin\release
+IF EXIST bin\relwithdebinfo\myodbc3i.exe SET bindir=bin\relwithdebinfo
 
 REM ****
 REM * Copying myodbc libraries and executables to install dir...
 REM ****
-ECHO Copying installation files
-IF NOT EXIST %libdir%\myodbc5.lib  GOTO :doError2
-IF NOT EXIST %libdir%\myodbc5S.lib GOTO :doError2
-IF NOT EXIST %bindir%\myodbc-installer.exe GOTO :doError2
-copy %libdir%\myodbc5S.dll %installdir%
-copy %libdir%\myodbc5S.lib %installdir%
-copy %libdir%\myodbc5.dll  %installdir%
-copy %libdir%\myodbc5.lib  %installdir%
-copy %bindir%\myodbc-installer.exe      %installdir%
+IF NOT EXIST %bindir%\myodbc3c.exe GOTO :doError2
+IF NOT EXIST %libdir%\myodbc3.lib  GOTO :doError2
+IF NOT EXIST %libdir%\myodbc3S.lib GOTO :doError2
+IF NOT EXIST %bindir%\myodbc3i.exe GOTO :doError2
+copy %libdir%\myodbc3S.dll %installdir%
+copy %libdir%\myodbc3S.lib %installdir%
+copy %libdir%\myodbc3.dll  %installdir%
+copy %libdir%\myodbc3.lib  %installdir%
+copy %bindir%\myodbc3i.exe      %installdir%
+IF EXIST %bindir%\myodbc3m.exe copy %bindir%\myodbc3m.exe      %installdir%
+copy %bindir%\myodbc3c.exe      %installdir%
+copy doc\*.hlp             %installdir%
 
 REM ****
 REM * Registering driver...
 REM *
-REM * We can do this with myodbc-installer.exe or the MS Windows ODBCConf.exe. It
+REM * We can do this with myodbc3i.exe or the MS Windows ODBCConf.exe. It
 REM * may be safer to use the ODBCConf.exe when we think about such things
 REM * as 64bit windows. 
 REM ****
-ECHO Registering driver
-myodbc-installer -d -a -n "MySQL ODBC 5.1 Driver" -t "DRIVER=myodbc5.dll;SETUP=myodbc5S.dll"
+myodbc3i -a -d -t"MySQL ODBC 3.51 Driver;DRIVER=myodbc3.dll;SETUP=myodbc3S.dll"
 
+GOTO doSuccess
+
+
+:doDebug
+REM ****
+REM * Find out the bin/lib directory, or use default
+REM ****
+SET libdir=lib
+IF EXIST lib\debug\myodbc3d.lib          SET libdir=lib\debug
+
+IF NOT EXIST %libdir%\myodbc3d.lib goto doError3
+IF NOT EXIST %libdir%\myodbc3E.lib goto doError3
+IF NOT EXIST %installdir%\myodbc3i.exe goto doError1
+REM ****
+REM * Copying myodbc debug libraries to install dir...
+REM ****
+copy %libdir%\myodbc3E.dll %installdir%
+copy %libdir%\myodbc3E.lib %installdir%
+copy %libdir%\myodbc3d.dll %installdir%
+copy %libdir%\myodbc3d.lib %installdir%
+
+REM ****
+REM * Registering driver...
+REM ****
+myodbc3i -a -d -t"MySQL ODBC 3.51 Driver (debug);DRIVER=myodbc3d.dll;SETUP=myodbc3E.dll"
+
+goto doSuccess
+
+
+:doSuccess
 ECHO "+-----------------------------------------------------+"
 ECHO "| DONE                                                |"
 ECHO "+-----------------------------------------------------+"
@@ -72,15 +106,37 @@ ECHO "|                                                     |"
 ECHO "+-----------------------------------------------------+"
 EXIT /B 0
 
+:doError1
+ECHO "+-----------------------------------------------------+"
+ECHO "| ERROR                                               |"
+ECHO "+-----------------------------------------------------+"
+ECHO "|                                                     |"
+ECHO "| The non-debug version of Connector/ODBC needs to be |"
+ECHO "| installed.                                          |"
+ECHO "|                                                     |"
+ECHO "+-----------------------------------------------------+"
+EXIT /B 1
+
 :doError2
 ECHO "+-----------------------------------------------------+"
 ECHO "| ERROR                                               |"
 ECHO "+-----------------------------------------------------+"
 ECHO "|                                                     |"
-ECHO "| Connector/ODBC not built.                           |"
+ECHO "| Connector/ODBC not built. Consider executing        |"
+ECHO "| Build.bat.                                          |"
 ECHO "|                                                     |"
 ECHO "+-----------------------------------------------------+"
-PAUSE
+EXIT /B 1
+
+:doError3
+ECHO "+-----------------------------------------------------+"
+ECHO "| ERROR                                               |"
+ECHO "+-----------------------------------------------------+"
+ECHO "|                                                     |"
+ECHO "| Connector/ODBC (debug) not built. Consider executing|"
+ECHO "| Build.bat.                                          |"
+ECHO "|                                                     |"
+ECHO "+-----------------------------------------------------+"
 EXIT /B 1
 
 :doError4
@@ -91,7 +147,6 @@ ECHO "|                                                     |"
 ECHO "| Existing Connector/ODBC installed. Request ignored. |"
 ECHO "|                                                     |"
 ECHO "+-----------------------------------------------------+"
-PAUSE
 EXIT /B 1
 
 :doError5
@@ -102,6 +157,28 @@ ECHO "|                                                     |"
 ECHO "| Can't find the Windows system directory             |"
 ECHO "|                                                     |"
 ECHO "+-----------------------------------------------------+"
-PAUSE
 EXIT /B 1
+
+:doSyntax
+ECHO "+-----------------------------------------------------+"
+ECHO "| Install.bat                                         |"
+ECHO "+-----------------------------------------------------+"
+ECHO "|                                                     |"
+ECHO "| DESCRIPTION                                         |"
+ECHO "|                                                     |"
+ECHO "| Use this to copy the driver and supporting files    |"
+ECHO "| to the system directory and register the driver.    |"
+ECHO "|                                                     |"
+ECHO "| You can not properly install the debug version      |"
+ECHO "| without first installing the regular version.       |"
+ECHO "|                                                     |"
+ECHO "| SYNTAX                                              |"
+ECHO "|                                                     |"
+ECHO "| Install <debug>                                     |"
+ECHO "|                                                     |"
+ECHO "| <debug>  must be;                                   |"
+ECHO "|              0 - to install a regular build         |"
+ECHO "|              1 - to install a debug version         |"
+ECHO "|                                                     |"
+ECHO "+-----------------------------------------------------+"
 
--- Uninstall.bat
+++ Uninstall.bat
@@ -6,9 +6,6 @@ REM
 REM         This exists for those working with the Windows source
 REM         distribution.
 REM
-REM         Use this to remove the driver and supporting files
-REM         from the system directory and deregister the driver.
-REM
 REM \sa     README.win
 REM
 REM #########################################################
@@ -18,21 +15,48 @@ IF EXIST %windir%\system\nul   SET installdir=%windir%\system
 IF EXIST %windir%\system32\nul SET installdir=%windir%\system32
 IF %installdir%==none GOTO :doError4
 
-IF NOT EXIST %installdir%\myodbc-installer.exe GOTO doError2
+IF "%1"=="0" GOTO doNormal
+IF "%1"=="1" GOTO doDebug
+GOTO doSyntax
+
+:doNormal
+IF NOT EXIST %installdir%\myodbc3i.exe GOTO doError2
 REM ****
 REM * Deregistering driver...
 REM ****
-myodbc-installer -d -r -n "MySQL ODBC 5.1 Driver"
+myodbc3i -r -d -n"MySQL ODBC 3.51 Driver"
 
 REM ****
 REM * Removing files...
 REM ****
-del /Q /F %installdir%\myodbc5S.dll
-del /Q /F %installdir%\myodbc5S.lib
-del /Q /F %installdir%\myodbc5.dll
-del /Q /F %installdir%\myodbc5.lib
-del /Q /F %installdir%\myodbc-installer.exe
+del /Q /F %installdir%\myodbc3S.dll
+del /Q /F %installdir%\myodbc3S.lib
+del /Q /F %installdir%\myodbc3.dll
+del /Q /F %installdir%\myodbc3.lib
+del /Q /F %installdir%\myodbc3i.exe
+del /Q /F %installdir%\myodbc3m.exe
+del /Q /F %installdir%\myodbc3c.exe
+del /Q /F %installdir%\myodbc3*.hlp
+GOTO doSuccess
 
+:doDebug
+IF NOT EXIST %installdir%\myodbc3d.dll GOTO doError3
+IF NOT EXIST %installdir%\myodbc3i.exe GOTO doError1
+REM ****
+REM * Deregistering driver...
+REM ****
+myodbc3i -r -d -n"MySQL ODBC 3.51 Driver (debug)"
+
+REM ****
+REM * Removing files...
+REM ****
+del /Q /F %installdir%\myodbc3E.dll
+del /Q /F %installdir%\myodbc3E.lib
+del /Q /F %installdir%\myodbc3d.dll
+del /Q /F %installdir%\myodbc3d.lib
+GOTO doSuccess
+
+:doSuccess
 ECHO "+-----------------------------------------------------+"
 ECHO "| DONE                                                |"
 ECHO "+-----------------------------------------------------+"
@@ -44,6 +68,17 @@ ECHO "|                                                     |"
 ECHO "+-----------------------------------------------------+"
 EXIT /B 0
 
+:doError1
+ECHO "+-----------------------------------------------------+"
+ECHO "| ERROR                                               |"
+ECHO "+-----------------------------------------------------+"
+ECHO "|                                                     |"
+ECHO "| The non-debug version of Connector/ODBC needs to be |"
+ECHO "| installed.                                          |"
+ECHO "|                                                     |"
+ECHO "+-----------------------------------------------------+"
+EXIT /B 1
+
 :doError2
 ECHO "+-----------------------------------------------------+"
 ECHO "| ERROR                                               |"
@@ -52,7 +87,17 @@ ECHO "|                                                     |"
 ECHO "| Connector/ODBC does not appear to be installed.     |"
 ECHO "|                                                     |"
 ECHO "+-----------------------------------------------------+"
-PAUSE
+EXIT /B 1
+
+:doError3
+ECHO "+-----------------------------------------------------+"
+ECHO "| ERROR                                               |"
+ECHO "+-----------------------------------------------------+"
+ECHO "|                                                     |"
+ECHO "| Connector/ODBC (debug) does not appear to be        |"
+ECHO "| installed.                                          |"
+ECHO "|                                                     |"
+ECHO "+-----------------------------------------------------+"
 EXIT /B 1
 
 :doError4
@@ -63,6 +108,28 @@ ECHO "|                                                     |"
 ECHO "| Can't find the Windows system directory             |"
 ECHO "|                                                     |"
 ECHO "+-----------------------------------------------------+"
-PAUSE
 EXIT /B 1
+
+:doSyntax
+ECHO "+-----------------------------------------------------+"
+ECHO "| Uninstall.bat                                       |"
+ECHO "+-----------------------------------------------------+"
+ECHO "|                                                     |"
+ECHO "| DESCRIPTION                                         |"
+ECHO "|                                                     |"
+ECHO "| Use this to remove the driver and supporting files  |"
+ECHO "| from the system directory and deregister the driver.|"
+ECHO "|                                                     |"
+ECHO "| The regular version must be installed for the       |"
+ECHO "| debug version to be properly uninstalled.           |"
+ECHO "|                                                     |"
+ECHO "| SYNTAX                                              |"
+ECHO "|                                                     |"
+ECHO "| Uninstall <debug>                                   |"
+ECHO "|                                                     |"
+ECHO "| <debug>  must be;                                   |"
+ECHO "|              0 - to uninstall a regular build       |"
+ECHO "|              1 - to uninstall a debug version       |"
+ECHO "|                                                     |"
+ECHO "+-----------------------------------------------------+"
 
