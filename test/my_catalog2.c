8e0ff7a54197e685d5258c025967cd2e1d3ba408
Bogdan Degtyariov <bogdan.degtyariov@oracle.com>
2013-06-25 17:21:17 +1000

    Test case for bug 16952531/69448 - MySQL ODBC drivers incorrectly quotes TableName when calling SQLPrimaryKeys

--- test/my_catalog2.c
+++ test/my_catalog2.c
@@ -1056,6 +1056,33 @@ DECLARE_TEST(t_sqlcolumns_after_select)
 }
 
 
+/* 
+  Bug #69448 MySQL ODBC drivers incorrectly quotes TableName when 
+  calling SQLPrimaryKeys
+*/
+DECLARE_TEST(t_bug69448)
+{
+ 
+  SQLCHAR buff[255];
+
+  ok_sql(hstmt, "DROP TABLE IF EXISTS `table``69448`");
+
+  ok_sql(hstmt, "CREATE TABLE `table``69448`(id int primary key)");
+
+  ok_stmt(hstmt, SQLPrimaryKeys(hstmt, NULL, SQL_NTS, NULL, SQL_NTS, 
+                               "table`69448", SQL_NTS));
+
+  ok_stmt(hstmt, SQLFetch(hstmt));
+
+  /* check the table name and PK name */
+  is_str(my_fetch_str(hstmt, buff, 3), "table`69448", 11);
+  is_str(my_fetch_str(hstmt, buff, 4), "id", 2);
+
+  ok_sql(hstmt, "DROP TABLE IF EXISTS `table``69448`");
+  return OK;
+}
+
+
 BEGIN_TESTS
   ADD_TEST(t_bug37621)
   ADD_TEST(t_bug34272)
@@ -1073,6 +1100,7 @@ BEGIN_TESTS
   ADD_TEST(t_bug14085211_part1)
   ADD_TODO(t_bug14085211_part2)
   ADD_TEST(t_sqlcolumns_after_select)
+  ADD_TODO(t_bug69448)
 END_TESTS
 
 myoption &= ~(1 << 30);
