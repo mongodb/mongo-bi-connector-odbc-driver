46679fe340b3ebcf052169eaabcc4a33357881fc
Bogdan Degtyariov <bogdan.degtyariov@oracle.com>
2013-06-26 21:45:55 +1000

    Fixed warning: parameter names (without types) in function declaration

--- driver/myutil.h
+++ driver/myutil.h
@@ -337,7 +337,7 @@ char * add_to_buffer(NET *net,char *to,const char *from,ulong length);
 MY_LIMIT_CLAUSE find_position4limit(CHARSET_INFO* cs, char *query,
                                     char * query_end);
 BOOL myodbc_isspace(CHARSET_INFO* cs, const char * begin, const char *end);
-BOOL got_out_parameters(stmt);
+BOOL got_out_parameters(STMT *stmt);
 
 /* handle.c*/
 BOOL          allocate_param_bind(DYNAMIC_ARRAY **param_bind, uint elements);
