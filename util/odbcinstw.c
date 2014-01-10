59c18cc2122fa0aaa28be4d9744aa9980d7341e3
Bogdan Degtyariov <bogdan.degtyariov@oracle.com>
2014-01-10 20:52:05 +1100

    Valgrind: jump or move depends on uninitialised value error from myodbc-installer (Bug# 18046120)

--- util/odbcinstw.c
+++ util/odbcinstw.c
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2000, 2012, Oracle and/or its affiliates. All rights reserved.
+  Copyright (c) 2000, 2014, Oracle and/or its affiliates. All rights reserved.
 
   The MySQL Connector/ODBC is licensed under the terms of the GPLv2
   <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most
@@ -67,7 +67,15 @@ MySQLGetPrivateProfileStringW(const MyODBC_LPCWSTR lpszSection, const MyODBC_LPC
   filename= (char *)sqlwchar_as_utf8(lpszFilename, &len);
 
   if (lpszRetBuffer && cbRetBuffer)
+  {
     ret= malloc(cbRetBuffer + 1);
+    /*
+      We cannot rely on getting correct results from the next call of
+      SQLGetPrivateProfileString. So, taking precautions against unititialized
+      values in the allocated buffer.
+    */
+    memset(ret, 0, cbRetBuffer + 1);
+  }
   else
     ret= NULL;
 
