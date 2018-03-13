1073e14b1ca474fabab42b0e293dfc22a8dab9e6
Patrick Meredith <pmeredit@gmail.com>
2018-03-13 13:38:33 -0400

    fix build script

--- bin/win32_odbc_build.sh
+++ bin/win32_odbc_build.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 SCRIPT_DIR=$(dirname $(readlink -f $0))
-BUILD_DIR=$SCRIPT_DIR/../build-win32
+ROOT=$SCRIPT_DIR/..
+BUILD_DIR=$ROOT/build-win32
 mkdir -p $BUILD_DIR
 S3_URL=https://s3.amazonaws.com/mciuploads/mongo-odbc-driver/prereqs/mysql-5.7.21-win32.zip
 DL_DIR=$BUILD_DIR/mysql-5.7.21-win32
 ZIP_FILE=mysql-32.zip
-ODBC_DIR=mysql-connector-odbc
+ODBC_DIR=$ROOT/src/mongo-odbc-driver/mysql-connector-odbc
 
 export PATH='/cygdrive/c/cmake/bin':'/cygdrive/c/Program Files (x86)/Microsoft Visual Studio 12.0/Common7/IDE':'/cygdrive/c/wixtools/bin':$PATH
 export MYSQL_DIR=$(cygpath -w $DL_DIR)
@@ -18,10 +19,6 @@ if [ ! -e $DL_DIR ]; then
     unzip $ZIP_FILE
 fi
 
-if [ ! -e $ODBC_DIR ]; then
-    git clone https://github.com/mysql/mysql-connector-odbc.git
-fi
-
 cd $ODBC_DIR
 
 #configure build
@@ -30,13 +27,13 @@ cmake -G "Visual Studio 12 2013" -DMYSQLCLIENT_STATIC_LINKING:BOOL=TRUE
 #build
 devenv.com MySQL_Connector_ODBC.sln /build Release
 
-cd ..
+cd $BUILD_DIR
 
 #we use cp -R because symlinks are problematic on cygwin
 cp -R ../resources/win32_installer/* ./
 
 #copy relevant files to installer installer dir
-FILE_DIR=$BUILD_DIR/mysql-connector-odbc/lib/Release
+FILE_DIR=$ODBC_DIR/lib/Release
 DEST_DIR=$BUILD_DIR/Files/File
 
 cp $FILE_DIR/myodbc5a.dll $DEST_DIR/myodbc5a.dll
