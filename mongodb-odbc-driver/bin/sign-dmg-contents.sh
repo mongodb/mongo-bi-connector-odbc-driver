8edebcb3720bb1d428df8d5c50e415721125633e
Patrick Meredith <pmeredit@gmail.com>
2018-05-18 15:36:38 -0400

    BI-1599: Sign macOS pkg file

---
+++ mongodb-odbc-driver/bin/sign-dmg-contents.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Copyright (c) 2018-Present MongoDB Inc.
+
+# shellcheck source=prepare-shell.sh
+. "$(dirname "$0")/prepare-shell.sh"
+
+DMG_SIGN_DIR="$PROJECT_ROOT/mongodb-odbc-driver/dmg-sign"
+rm -Rf "$DMG_SIGN_DIR"
+mkdir -p "$DMG_SIGN_DIR"
+
+cd "$DMG_SIGN_DIR"
+cp -R "$PROJECT_ROOT/mongodb-odbc-driver/installer/dmg/dmg-contents" ./
+
+hdiutil detach /Volumnes/mongodb-odbc || true
+hdiutil attach "$PKG_DIR"/mongodb-odbc.dmg
+
+PKG="mongodb-connector-odbc-$MDBODBC_VER-macos-x86-64.pkg"
+cp /Volumes/mongodb-odbc/"$PKG" ./
+# The developer ID will be passed as an argument to this script.
+productsign --sign "$1" "$PKG" ./dmg-contents/"$PKG"
+
+hdiutil create -fs HFS+ -srcfolder dmg-contents -volname mongodb-odbc mongodb-odbc-signed.dmg
+
+cp ./*.dmg "$PKG_DIR"/
