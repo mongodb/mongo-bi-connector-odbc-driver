2f2ba1f0c7d5a4148422ba33c995134343df7652
mci <mci@macos-1012-99.macstadium.build.10gen.cc>
2020-05-16 03:20:08 +0000

    BI-2405: put openssl libs in the main directory

--- mongodb-odbc-driver/bin/create-dmg.sh
+++ mongodb-odbc-driver/bin/create-dmg.sh
@@ -23,9 +23,8 @@
 	# copy driver libraries to appropriate location
 	cp "$DRIVERS_DIR"/*.so ./
 	# copy openssl to appropriate location
-	mkdir -p ./openssl/lib
-	cp "/usr/local/Cellar/openssl/1.0.2r/lib/libcrypto.1.0.0.dylib" ./openssl/lib/
-	cp "/usr/local/Cellar/openssl/1.0.2r/lib/libssl.1.0.0.dylib" ./openssl/lib/
+	cp "/usr/local/Cellar/openssl/1.0.2r/lib/libcrypto.1.0.0.dylib" ./
+	cp "/usr/local/Cellar/openssl/1.0.2r/lib/libssl.1.0.0.dylib" ./
 
 	sh ./build-dmg.sh
 
