2c3c1b2906418329d0767216adcad15e2a72ca74
Patrick Meredith <pmeredit@gmail.com>
2020-05-15 22:16:29 -0400

    BI-2405: change os x rpaths

--- mongodb-odbc-driver/bin/build-mongo-odbc.sh
+++ mongodb-odbc-driver/bin/build-mongo-odbc.sh
@@ -5,7 +5,7 @@
 . "$(dirname "$0")/prepare-shell.sh"
 
 (
-	set -o errexit
+    set -o errexit
     # clear the BUILD_DIR
     echo "clear $BUILD_DIR"
     rm -rf "$BUILD_DIR"
@@ -78,10 +78,17 @@
     if [ "$PLATFORM_NAME" = "windows" ]; then
         cp "$BUILD_DIR"/lib/Release/mdbodbc{a,S,w}.{dll,lib} "$DRIVERS_DIR"
         cp "$BUILD_DIR"/bin/Release/myodbc-installer.exe "$DRIVERS_DIR"/installer.exe
-		#also copy openssl
-		cp "$OPENSSL_PATH"/*.dll "$DRIVERS_DIR"
+        #also copy openssl
+        cp "$OPENSSL_PATH"/*.dll "$DRIVERS_DIR"
     else
         cp "$BUILD_DIR"/lib/libmdbodbc{a,w}.so "$DRIVERS_DIR"
+        # if this is mac, we need to update the openssl library rpaths
+        if [ "$PLATFORM_NAME" = "macos" ]; then
+            install_name_tool -change /usr/local/opt/openssl/lib/libssl.1.0.0.dylib ./libssl.1.0.0.dylib "$DRIVERS_DIR"/libmdbodbca.so
+            install_name_tool -change /usr/local/opt/openssl/lib/libssl.1.0.0.dylib ./libssl.1.0.0.dylib "$DRIVERS_DIR"/libmdbodbcw.so
+            install_name_tool -change /usr/local/opt/opencrypto/lib/libcrypto.1.0.0.dylib ./libcrypto.1.0.0.dylib "$DRIVERS_DIR"/libmdbodbca.so
+            install_name_tool -change /usr/local/opt/opencrypto/lib/libcrypto.1.0.0.dylib ./libcrypto.1.0.0.dylib "$DRIVERS_DIR"/libmdbodbcw.so
+        fi
     fi
 ) > $LOG_FILE 2>&1
 
