396a458b260ef4a153c2235b0684b9c3d1f19c56
Patrick Meredith <pmeredit@gmail.com>
2020-05-18 13:57:31 -0400

    BI-2405: Fix tabs again, bad vim, bad

--- mongodb-odbc-driver/bin/create-dmg.sh
+++ mongodb-odbc-driver/bin/create-dmg.sh
@@ -5,34 +5,34 @@
 . "$(dirname "$0")/prepare-shell.sh"
 
 (
-	if [ "$PLATFORM_NAME" != macos ]; then
-	    exit 0
-	fi
+    if [ "$PLATFORM_NAME" != macos ]; then
+        exit 0
+    fi
 
-	export PREFIX=mongodb-connector-odbc-"$MDBODBC_VER"-macos-x86-64
+    export PREFIX=mongodb-connector-odbc-"$MDBODBC_VER"-macos-x86-64
 
-	# clear DMG_BUILD_DIR
-	rm -rf "$DMG_BUILD_DIR"
-	mkdir -p "$DMG_BUILD_DIR"
+    # clear DMG_BUILD_DIR
+    rm -rf "$DMG_BUILD_DIR"
+    mkdir -p "$DMG_BUILD_DIR"
 
-	cd "$DMG_BUILD_DIR"
+    cd "$DMG_BUILD_DIR"
 
-	# copy dmg sources to current directory
-	cp -R "$PROJECT_ROOT/mongodb-odbc-driver/installer/dmg/"* ./
+    # copy dmg sources to current directory
+    cp -R "$PROJECT_ROOT/mongodb-odbc-driver/installer/dmg/"* ./
 
-	# copy driver libraries to appropriate location
-	cp "$DRIVERS_DIR"/*.so ./
-	# copy the openssl libs to appropriate location
+    # copy driver libraries to appropriate location
+    cp "$DRIVERS_DIR"/*.so ./
+    # copy the openssl libs to appropriate location
     cp "$BREW_OPENSSL_PATH/lib{ssl,crypto}.1.0.0.dylib"  ./
 
-	sh ./build-dmg.sh
+    sh ./build-dmg.sh
 
-	# clear PKG_DIR
-	rm -rf "$PKG_DIR"
-	mkdir -p "$PKG_DIR"
+    # clear PKG_DIR
+    rm -rf "$PKG_DIR"
+    mkdir -p "$PKG_DIR"
 
-	# copy the dmg to PKG_DIR
-	cp ./*.dmg "$PKG_DIR"/
+    # copy the dmg to PKG_DIR
+    cp ./*.dmg "$PKG_DIR"/
 ) > $LOG_FILE 2>&1
 
 print_exit_msg
