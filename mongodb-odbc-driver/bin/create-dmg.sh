4b5d6a79ecedf27eba3a6375070415f093e16fcb
Patrick Meredith <pmeredit@gmail.com>
2023-01-10 00:50:45 -0500

    BI-2668: correctly copy dylibs

--- mongodb-odbc-driver/bin/create-dmg.sh
+++ mongodb-odbc-driver/bin/create-dmg.sh
@@ -31,7 +31,7 @@
     cp "$OPENSSL_PATH/libcrypto.1.0.0.dylib"  ./
 
     chmod 777 libssl.1.0.0.dylib
-    install_name_tool -change "$OPENSSL_PATH/libcrypto.1.0.0.dylib" "@loader_path/libcrypto.1.0.0.dylib" ./libssl.1.0.0.dylib
+    install_name_tool -change "/usr/local/Cellar/openssl/1.0.2n/lib/libcrypto.1.0.0.dylib" "@loader_path/libcrypto.1.0.0.dylib" ./libssl.1.0.0.dylib
 
     sh ./build-dmg.sh
 
